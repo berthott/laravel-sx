d5e74dd91347426ca50b05fcb41d9001a0b8adec
Jan Bladt <jan.bladt@syspons.com>
2021-10-14 11:50:44 +0200

    add colum checks

--- tests/Feature/Sxable/SxableTest.php
+++ tests/Feature/Sxable/SxableTest.php
@@ -18,7 +18,16 @@ class SxableTest extends SxableTestCase
     {
         Sxable::getSxableClasses();
         $this->assertTrue(Schema::hasTable('entities'));
+        $this->assertTrue(Schema::hasColumns('entities', [
+            'survey', 'responde', 'organiza', 'stat_1', 'stat_2', 'stat_3', 'stat_4', 'stat_5', 'stat_6', 'stat_7', 'stat_8', 'stat_9', 'stat_10', 'stat_11', 'stat_12', 'stat_13', 'stat_14', 'stat_15', 'stat_16', 'stat_17', 'stat_18', 'stat_19', 'stat_20', 'created', 'modified', 'closetim', 'starttim', 'difftime', 'response', 'numberof', 'importgr', 'distribu', 'email', 'digitald', 'digit_1', 's_5', 's_2', 's_3', 'name', 's_4', 's_1', 's_6', 's_7', 's_14', 's_9', 's_15', 's_10', 's_11_1', 's_11_2', 's_11_3', 's_11_4', 's_11_5', 's_11_6', 's_11_7', 's_11_8', 's_11_9', 's_11_10', 's_11_11', 's_11_12', 's_11_13', 's_11_14', 's_11_15', 's_11_16', 's_11_17', 's_12', 's_17', 's_18', 's_19', 's_20', 's_21', 's_22', 's_23', 's_24', 'lang', 'statc_1', 'statc_2', 'statc_3', 'statc_4', 'statc_5', 'statc_6', 'statc_7', 'statc_8', 'statc_9', 'stat_21', 'statd_1', 'statd_2', 'statd_3', 'statd_4', 'statd_5', 'stats_1', 'stats_2', 'stats_3', 'stats_4', 'stats_5', 'stats_6', 'stat_22', 'stat_23', 'stat_24', 'stat_25', 'stato_1', 'stato_2', 'stato_3', 'stato_4', 'stato_5',
+        ]));
         $this->assertTrue(Schema::hasTable('entity_values'));
+        $this->assertTrue(Schema::hasColumns('entity_values', [
+            'questionName', 'variableName', 'choiceValue', 'choiceText',
+        ]));
         $this->assertTrue(Schema::hasTable('entity_questions'));
+        $this->assertTrue(Schema::hasColumns('entity_questions', [
+            'questionName', 'questionText',
+        ]));
     }
 }

f4d5116e7c6bf16709ea3b862dc0815a0eb9495f
Jan Bladt <jan.bladt@syspons.com>
2022-12-12 14:37:45 +0100

    table name comes from crudable

--- src/Models/Traits/SxDistributable.php
+++ src/Models/Traits/SxDistributable.php
@@ -4,25 +4,25 @@ namespace berthott\SX\Models\Traits;
 
 use berthott\InternalRequest\Facades\InternalRequest;
 use berthott\SX\Models\Respondent;
-use Illuminate\Support\Str;
+//use Illuminate\Support\Str;
 
 trait SxDistributable
 {
     /**
      * The single name of the model.
      */
-    public static function singleName(): string
+    /* public static function singleName(): string
     {
         return Str::snake(class_basename(get_called_class()));
-    }
+    } */
 
     /**
      * The entity table name of the model.
      */
-    public static function entityTableName(): string
+    /* public static function entityTableName(): string
     {
         return Str::snake(Str::pluralStudly(class_basename(get_called_class())));
-    }
+    } */
 
     /**
      * The class of the sxable to collect.

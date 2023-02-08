0fb3cf72978a478280b48c00adcef38b109b08d9
Jan Bladt <jan.bladt@syspons.com>
2023-02-08 13:48:41 +0100

    remove unused wide report

--- src/SxServiceProvider.php
+++ src/SxServiceProvider.php
@@ -19,7 +19,6 @@ use berthott\SX\Services\Http\SxEntityService;
 use berthott\SX\Services\SxableService;
 use berthott\SX\Services\SxDistributableService;
 use berthott\SX\Services\SxReportLongService;
-use berthott\SX\Services\SxReportWideService;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Routing\Router;

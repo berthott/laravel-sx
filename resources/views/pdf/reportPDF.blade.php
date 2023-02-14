49f886c546b76515d0a41f99252f3478ef537d30
Or Sarfati <or.sarfati@syspons.com>
2023-02-14 12:21:42 +0100

    add pdf blade view

---
+++ resources/views/pdf/reportPDF.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+  <head>
+    <title>QR Code</title>
+    <style>
+      html, 
+      body, 
+      img, 
+      h1 {
+        width: 100%;
+      }
+
+      .page-break {
+        page-break-after: always;
+      }
+
+      .debug {
+        margin: 0 auto;
+        max-width: 100%;
+        border: solid 1px red;
+        padding: 12px;
+        overflow-wrap: break-word;
+        word-wrap: break-word;
+        hyphens: auto;
+      }
+
+    </style>
+  </head>
+  <body>
+    <div style="width:100%">
+      @foreach ($data as $layoutGroup)
+          <h1>{!! $layoutGroup['title'] !!}</h1>
+          @foreach ($layoutGroup['images'] as $src)
+            <img src="{!! $src !!}" />
+          @endforeach
+          <div class="page-break"></div>
+      @endforeach
+    </div>
+  </body>
+</html>
